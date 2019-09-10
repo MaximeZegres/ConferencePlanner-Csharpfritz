778d7cee150a0b5f50b3ac43673dac8bd9e52b0f
maxime.zegres <maxime.zegres@gmail.com>
2019-09-10 22:29:29 +0200

    - Add scaffolded SpeakersController - Add ConferenceDataObjectTransfer project

--- BackEnd/Models/Speaker.cs
+++ BackEnd/Models/Speaker.cs
@@ -6,18 +6,8 @@ using System.Threading.Tasks;
 
 namespace BackEnd.Models
 {
-    public class Speaker
+    public class Speaker : ConferenceDTO.Speaker
     {
-        public int ID { get; set; }
 
-        [Required]
-        [StringLength(200)]
-        public string Name { get; set; }
-
-        [StringLength(4000)]
-        public string Bio { get; set; }
-
-        [StringLength(1000)]
-        public virtual string WebSite { get; set; }
     }
 }
